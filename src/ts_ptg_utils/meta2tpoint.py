d2d26f1a20905950d58e20ce4c5421fd0fbe3b35
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2026-03-04 10:47:06 -0700

    fixup! Add script ``meta2tpoint`` that reduces on sky data into tpoint data file.

--- src/ts_ptg_utils/meta2tpoint.py
+++ src/ts_ptg_utils/meta2tpoint.py
@@ -49,7 +49,7 @@ def main(
     metadata_dir: str,
     instrument: str,
 ) -> None:
-    asyncio.run(amain())
+    asyncio.run(amain(day_obs=day_obs, program=program,metadata_dir=metadata_dir, instrument=instrument))
 
 async def amain(
     day_obs: int,
